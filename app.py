2794194e91af4683e4a78e665fc450dc5a1e0320
meokgmblog <meokgmblog@gmail.com>
2026-08-26 13:55:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -314,6 +314,8 @@ try:
             ce_opts = atm_opts[atm_opts[sym_col].astype(str).str.endswith("CE")]
             pe_opts = atm_opts[atm_opts[sym_col].astype(str).str.endswith("PE")]
 
+
+
             ce_df = None
             for _, row in ce_opts.iterrows():
                 opt_data = filter_market_hours(get_derivative_intraday(ACCESS_TOKEN, row[key_col]))
@@ -324,7 +326,7 @@ try:
                     else:
                         ce_df = pd.merge(ce_df, opt_sub, on="timestamp", how="outer")
                         ce_df["ce_oi"] = ce_df["ce_oi"].fillna(0) + ce_df["oi"].fillna(0)
-                        ce_df.drop(columns={"oi"], inplace=True)
+                        ce_df.drop(columns=["oi"], inplace=True)
 
             pe_df = None
             for _, row in pe_opts.iterrows():
@@ -336,7 +338,7 @@ try:
                     else:
                         pe_df = pd.merge(pe_df, opt_sub, on="timestamp", how="outer")
                         pe_df["pe_oi"] = pe_df["pe_oi"].fillna(0) + pe_df["oi"].fillna(0)
-                        pe_df.drop(columns={"oi"], inplace=True)
+                        pe_df.drop(columns=["oi"], inplace=True)
 
             if ce_df is not None and pe_df is not None:
                 ce_df = ce_df.sort_values("timestamp").ffill().dropna()
